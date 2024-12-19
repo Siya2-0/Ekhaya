bf0fa169121a6f7b10b21af4bcec8dc5dc5038a6
Siyabonga Mbuyisa-20491621 <160500265+Siyabonga-Mbuyisa-u20491621@users.noreply.github.com>
2024-12-19 15:42:40 +0200

    Added Authenication function

--- ekhaya-bar-lounge-app/main.js
+++ ekhaya-bar-lounge-app/main.js
@@ -1,13 +1,21 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path');
 
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,//change window size
-      height: 600
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
     })
   
     win.loadFile('index.html')
   }
+
+  ipcMain.on('navigate-to-second-page', () => {
+    mainWindow.loadFile('second.html');
+  });
 
 
   app.whenReady().then(() => {
